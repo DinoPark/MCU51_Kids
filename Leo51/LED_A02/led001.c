06b0e8228cc77ffe7056676619c9f03e5e236b96
Leo <rubberman919@gmail.com>
2012-06-22 21:06:33 +0800

    first try of LED running

---
+++ Leo51/LED_A02/led001.c
@@ -0,0 +1,27 @@
+#include <STC89.H>
+
+void main(void)
+{
+	unsigned char t;
+
+	P0=0x7e;
+	for(t=0; t<3000; t++);
+	
+	P0=0xbd;
+	for(t=0; t<4000; t++);
+	
+	P0=0xdb;
+	for(t=0; t<6000; t++);
+	
+	P0=0xe7;
+	for(t=0; t<9000; t++);
+	
+	P0=0xdb;
+	for(t=0; t<6000; t++);
+	
+	P0=0xbd;
+	for(t=0; t<4000; t++);
+	
+	P0=0x7e;
+	for(t=0; t<3000; t++);
+}

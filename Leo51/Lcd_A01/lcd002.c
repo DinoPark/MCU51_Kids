27cfbefedf6a7eeaa3bd3167072f384162890fca
Leo <rubberman919@gmail.com>
2012-07-03 19:02:26 +0800

    Lcd test

--- Leo51/Lcd_A01/lcd002.c
+++ Leo51/Lcd_A01/lcd002.c
@@ -85,17 +85,34 @@ void main(void)
 	lcdWriteCmd(0x38);
 	lcdWriteCmd(0x06);
 
-	lcdWriteData(0x46);
-	lcdWriteData(0x20);
-	lcdWriteData(0x4C);
-	lcdWriteData(0x6F);
-	lcdWriteData(0x76);
-	lcdWriteData(0x65);
-	lcdWriteData(0x20);
-	lcdWriteData(0x38);
-	lcdWriteData(0x30);
-	lcdWriteData(0x35);
-	lcdWriteData(0x31);
+	lcdWriteData('I');				//'I'//
+	lcdWriteData(' ');				//'	'//
+	lcdWriteData('L');				//'L'//
+	lcdWriteData('o');				//'o'//
+	lcdWriteData('v');				//'v'//
+	lcdWriteData('e');				//'e'//
+	lcdWriteData(' ');				//'	'//
+	lcdWriteData('8');				//'8'//
+	lcdWriteData('0');				//'0'//
+	lcdWriteData('5');				//'5'//
+	lcdWriteData('1');				//'1'//
+	lcdWriteData(0x7E);				//'->'//
+	lcdWriteData(0xF6);				//'	'//
+	lcdWriteData(0x33);				//'3'//
+	lcdWriteData(0xFA);			    //'Ç§'//
+	lcdWriteData(0xFB);				//'Íò'//
+
+//	lcdWriteData(0x46);
+//	lcdWriteData(0x20);
+//	lcdWriteData(0x4C);
+//	lcdWriteData(0x6F);
+//	lcdWriteData(0x76);
+//	lcdWriteData(0x65);
+//	lcdWriteData(0x20);
+//	lcdWriteData(0x38);
+//	lcdWriteData(0x30);
+//	lcdWriteData(0x35);
+//	lcdWriteData(0x31);
 
 	for (;;);
 }
